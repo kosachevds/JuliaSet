1ee927a61dbfc4dfcd49928d7298656357eaa601
Daniil Kosachev <kos-dan@mail.ru>
2019-03-26 20:20:03 +0300

    Added class JuliaSet

---
+++ JuliaSet.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+
+namespace JuliaSet
+{
+    class JuliaSet
+    {
+        public Complex CValue { get; }
+
+        public JuliaSet(double cValue)
+        {
+            this.CValue = cValue;
+        }
+
+    }
+}
