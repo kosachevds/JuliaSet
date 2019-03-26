c7489259dc3a2ef481e7163e465f04f1bbb98245
Daniil Kosachev <kos-dan@mail.ru>
2019-03-26 21:08:04 +0300

    Added methods PlotOnBitmap, GetColor

--- JuliaSet.cs
+++ JuliaSet.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Drawing;
 using System.Numerics;
 
 namespace JuliaSet
 {
     class JuliaSet
     {
+        private const int MaxThreadCount = 12;
+
         public Complex CValue { get; }
 
         public JuliaSet(double cValue)
@@ -15,6 +18,31 @@ namespace JuliaSet
         public void Create(string filename, int maxIteration, int width, int height)
         {
 
+        }
+
+        private void PlotOnBitmap(Bitmap bitmap, int maxIteration)
+        {
+            var rValue = this.ComputeRValue();
+
+            var realMin = -rValue;
+            var realMax = rValue;
+            var realStep = (realMax - realMin) / bitmap.Width;
+
+            var imagMin = -rValue;
+            var imagMax = rValue;
+            var imapStep = (imagMax - imagMin) / bitmap.Height;
+
+            for (int i = 0; i < bitmap.Width; ++i) {
+                var xCoordinate = bitmap.Width - i - 1;
+                var real = realMin + i * realStep;
+                for (int j = 0; j < bitmap.Height; ++j) {
+                    var imag = realMin + j * imapStep;
+                    var zij = new Complex(real, imag);
+                    var count = CountIterations(ref zij, maxIteration, rValue);
+                    var ratioZR = Complex.Abs(zij) / rValue;
+                    bitmap.SetPixel(xCoordinate, j, GetColor(count, maxIteration, ratioZR));
+                }
+            }
         }
 
         private int CountIterations(ref Complex initialZ, int maxIteration, double rValue)
@@ -32,6 +60,17 @@ namespace JuliaSet
         private double ComputeRValue()
         {
             return (1 + Math.Sqrt(1 + 4 * Complex.Abs(this.CValue))) / 2;
+        }
+
+        private static Color GetColor(int count, int maxIteration, double ratioZR)
+        {
+            var countRatio = (double)count / maxIteration;
+            return Color.FromArgb(
+                255,
+                Convert.ToByte(255 * countRatio),
+                Convert.ToByte(255 * (1 - countRatio)),
+                Convert.ToByte(255 * Math.Max(1, ratioZR))
+            );
         }
     }
 }
