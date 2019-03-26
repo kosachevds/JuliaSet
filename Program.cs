4d73811f2df28b07582de44167fadbdf9d45c012
Daniil Kosachev <kos-dan@mail.ru>
2019-03-26 21:24:26 +0300

    Lanching code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace JuliaSet
 {
@@ -6,6 +7,15 @@ namespace JuliaSet
     {
         static void Main(string[] args)
         {
+            var juliaSet = new JuliaSet(new System.Numerics.Complex(-0.8, 0.156));
+            var sw = new Stopwatch();
+
+            sw.Start();
+            juliaSet.Create("juliaSet.bmp", 100, 5000, 5000);
+            sw.Stop();
+
+            Console.WriteLine("Elapsed: {0}, sec", sw.ElapsedMilliseconds / 1000.0);
+
         }
     }
 }
