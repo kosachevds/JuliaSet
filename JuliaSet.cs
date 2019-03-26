28f44cb54bf6f92be9164ff6592df2fbb67c0bc0
Daniil Kosachev <kos-dan@mail.ru>
2019-03-26 20:31:17 +0300

    Added method CountIterations

--- JuliaSet.cs
+++ JuliaSet.cs
@@ -17,6 +17,18 @@ namespace JuliaSet
 
         }
 
+        private int CountIterations(ref Complex initialZ, int maxIteration, double rValue)
+        {
+            var count = 0;
+            var lastZ = initialZ;
+            while (count < maxIteration && Complex.Abs(lastZ) > 0)
+            {
+                lastZ = lastZ * lastZ + this.CValue;
+                ++count;
+            }
+            return count;
+        }
+
         private double ComputeRValue()
         {
             return (1 + Math.Sqrt(1 + 4 * Complex.Abs(this.CValue))) / 2;
